c77aba9e991594fe7d5800cba955c66ed798ecc5
Liam McArdle <mcardle.liam@gmail.com>
2013-09-13 11:56:54 -0700

    add ember to project

---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,5 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+EmberBloger.Router.map ()->
+  # @resource('posts')
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ EmberBloger::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.ember.variant = :development
 end
